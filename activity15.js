42b255bd6fe6dc6433dea388dc2aee2cabae2290
rai-rishi <119608553+rai-rishi@users.noreply.github.com>
2022-12-29 23:57:37 +0530

    Update activity15.js

--- activity15.js
+++ activity15.js
@@ -1,33 +1,14 @@
-let type = document.querySelector("header");
-console.log(type);
+// Single Line comment
 
-let classSelector = document.querySelector("header-content");
-console.log(classSelector);
+/*Multi 
+line
+comment*/
 
-let idSelector = document.querySelector("#header");
-console.log(idSelector);
-
-let decendent = document.querySelector(".header-content");
-console.log(decendent);
-
-let All = document.querySelector("main p");
-console.log(All);
-
-let bg = document
-  .querySelector("main p")
-  .forEach((item) => (item.style.backgroundColor = "blue"));
-
-let color = document
-  .querySelector("main p:last-child")
-  .forEach((item) => (item.style.backgroundColor = "White"));
-
-// Older Methods
-
-let oldtype = document.getElementsByTagName("header");
-console.log(oldclass);
-
-let oldclass = document.getElementsByClassName("header-content");
-console.log(oldclass);
-
-let oldid = document.getElementsById("header");
-console.log(oldid);
+const navSlide = () => {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav-links");
+  const navlinks = document.querySelectorAll(".nav-links");
+  burger.addEventListener("click",() =>{
+  //Toggle Nav
+  nav.classList.toggle("nav-active");)}
+};
